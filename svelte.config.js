9060f5c418ca1719c401d3e862ad056b9f172a72
sebas <sebdeveloper6952@gmail.com>
2024-10-09 23:17:45 -0600

    Add app base path to match server.

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,10 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
             fallback: "200.html",
-        })
+        }),
+        paths: {
+            base: "/ui",
+        },
     }
 };
 
